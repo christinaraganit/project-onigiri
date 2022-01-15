6827a114048e8f6b08dc69e735fd0f4bfc43a48f
Christina <christinaraganit@gmail.com>
2022-01-15 01:24:45 -0800

    >:3

--- onigiri/src/app/app.module.ts
+++ onigiri/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +17,13 @@ import { MaterialModule } from './material/material.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MaterialModule,
+    RouterModule.forRoot(
+      [{path: 'dashboard', component: DashboardComponent},
+       {path: '', redirectTo: '/dashboard', pathMatch: 'full' },      ]
+    )
+  ],
+  exports: [
     MaterialModule
   ],
   providers: [],
