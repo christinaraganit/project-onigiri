23ede0e2b060a6820ca88142e4d4b2eab3f8fe8e
Christina <christinaraganit@gmail.com>
2022-01-15 17:11:50 -0800

    firebase added + sign up and login files made

--- onigiri/src/environments/environment.prod.ts
+++ onigiri/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'onigiri-app',
+    appId: '1:875030966525:web:a07b9d50a0952a7cd5ce94',
+    storageBucket: 'onigiri-app.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAVOTwYBF7ajPXkYxlUI5E9TC0fau981Hc',
+    authDomain: 'onigiri-app.firebaseapp.com',
+    messagingSenderId: '875030966525',
+  },
   production: true
 };
