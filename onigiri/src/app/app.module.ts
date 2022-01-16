ac064d2826d24ac1e7bdcc7b0d26c6784a04a9a1
Christina <christinaraganit@gmail.com>
2022-01-15 19:04:13 -0800

    Revert "firebase added + sign up and login files made"
    This reverts commit 23ede0e2b060a6820ca88142e4d4b2eab3f8fe8e.

--- onigiri/src/app/app.module.ts
+++ onigiri/src/app/app.module.ts
@@ -10,27 +10,11 @@ import { MaterialModule } from './material/material.module';
 import { LandingPageComponent  } from './landing-page/landing-page.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
-import { environment } from 'src/environments/environment';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideDatabase,getDatabase } from '@angular/fire/database';
-import { provideFunctions,getFunctions } from '@angular/fire/functions';
-import { provideMessaging,getMessaging } from '@angular/fire/messaging';
-import { providePerformance,getPerformance } from '@angular/fire/performance';
-import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
-import { provideStorage,getStorage } from '@angular/fire/storage';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
-
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    LandingPageComponent,
-    LoginComponent,
-    SignupComponent
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
@@ -40,24 +24,12 @@ import { SignupComponent } from './signup/signup.component';
     RouterModule.forRoot(
       [{path: 'dashboard', component: DashboardComponent},
       {path: '', component: LandingPageComponent},]
-    ),
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAnalytics(() => getAnalytics()),
-    provideAuth(() => getAuth()),
-    provideDatabase(() => getDatabase()),
-    provideFirestore(() => getFirestore()),
-    provideFunctions(() => getFunctions()),
-    provideMessaging(() => getMessaging()),
-    providePerformance(() => getPerformance()),
-    provideRemoteConfig(() => getRemoteConfig()),
-    provideStorage(() => getStorage()),
+    )
   ],
   exports: [
     MaterialModule
   ],
-  providers: [
-    ScreenTrackingService,UserTrackingService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- onigiri/src/environments/environment.ts
+++ onigiri/src/environments/environment.ts
@@ -2,35 +2,8 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAVOTwYBF7ajPXkYxlUI5E9TC0fau981Hc",
-  authDomain: "onigiri-app.firebaseapp.com",
-  projectId: "onigiri-app",
-  storageBucket: "onigiri-app.appspot.com",
-  messagingSenderId: "875030966525",
-  appId: "1:875030966525:web:a07b9d50a0952a7cd5ce94"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
 export const environment = {
-  production: false,
-  firebase: {
-    projectId: 'onigiri-app',
-    appId: '1:875030966525:web:a07b9d50a0952a7cd5ce94',
-    storageBucket: 'onigiri-app.appspot.com',
-    locationId: 'us-central',
-    apiKey: 'AIzaSyAVOTwYBF7ajPXkYxlUI5E9TC0fau981Hc',
-    authDomain: 'onigiri-app.firebaseapp.com',
-    messagingSenderId: '875030966525',
-  }
+  production: false
 };
 
 /*
