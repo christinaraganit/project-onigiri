db582aa1ff92e1e82fd9e12af1679b1394ad962f
Eric Dam <dam.eric3@gmail.com>
2022-01-22 21:42:19 -0800

    just in case

--- onigiri/src/environments/environment.prod.ts
+++ onigiri/src/environments/environment.prod.ts
@@ -9,5 +9,14 @@ export const environment = {
     authDomain: 'onigiri-app.firebaseapp.com',
     messagingSenderId: '875030966525',
   },
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAVOTwYBF7ajPXkYxlUI5E9TC0fau981Hc",
+    authDomain: "onigiri-app.firebaseapp.com",
+    databaseURL: "https://onigiri-app-default-rtdb.firebaseio.com",
+    projectId: "onigiri-app",
+    storageBucket: "onigiri-app.appspot.com",
+    messagingSenderId: "875030966525",
+    appId: "1:875030966525:web:a07b9d50a0952a7cd5ce94"
+  }
 };
